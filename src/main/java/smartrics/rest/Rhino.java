eb65f94099ea0fdf7ee4c88f7cc2f78186eb741f
Fabrizio Cannizzo <smartrics@gmail.com>
2011-04-11 18:45:24 +0100

    added few interesting cases on Rhino spike

--- src/main/java/smartrics/rest/Rhino.java
+++ src/main/java/smartrics/rest/Rhino.java
@@ -2,6 +2,9 @@ package smartrics.rest;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+
+import smartrics.rest.fitnesse.fixture.support.Variables;
 
 /**
  * a.
@@ -19,8 +22,17 @@ public class Rhino {
         try {
             Context cx = Context.enter();
             Scriptable scope = cx.initStandardObjects();
-            Object result = cx.evaluateString(scope, "json={\"address\" : \"rue de la mignot\" }\njson.address.xxx", null, 1, null);
-            System.out.println(Context.toString(result));
+            Object result = cx.evaluateString(scope, "json={\"address\" : \"regent street\" }\njson.address == 'regent street'", null, 1, null);
+            System.out.println("access json object with dot notation:\n" + Context.toString(result));
+
+            Variables v = new Variables();
+            v.put("fab1", "1234");
+            Object wrappedVariables = Context.javaToJS(v, scope);
+            ScriptableObject.putProperty(scope, "symbols", wrappedVariables);
+            v.put("fab2", "4321");
+            result = cx.evaluateString(scope, "symbols.get('fab1')+' '+symbols.get('fab2')", null, 1, null);
+            System.out.println("wrap fit symbols:\n" + Context.toString(result));
+
         } finally {
             Context.exit();
         }
