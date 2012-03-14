6b1732c341be4dd05f7818debf359ba8d5f47572
Fabrizio Cannizzo <smartrics@gmail.com>
2012-03-14 23:51:24 +0000

    fixed slim support for seq diagrams - by adding a new class

--- src/test/java/smartrics/rest/fitnesse/fixture/RestFixtureWithSeqFitTest.java
+++ src/test/java/smartrics/rest/fitnesse/fixture/RestFixtureWithSeqFitTest.java
@@ -25,12 +25,15 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
 
 import smartrics.rest.client.RestClient;
 import smartrics.rest.client.RestRequest;
@@ -64,6 +67,12 @@ public class RestFixtureWithSeqFitTest {
     private RestResponse lastResponse;
     private Model mockModel;
 
+    class IsEmbeddedImageEncodedInBase64 extends ArgumentMatcher<String> {
+        public boolean matches(Object arg) {
+            return arg.toString().startsWith("<img src=\"data:image/svg;base64,");
+        }
+     }    
+    
     @Before
     public void setUp() {
         helper = new RestFixtureTestHelper();
@@ -128,104 +137,17 @@ public class RestFixtureWithSeqFitTest {
 
     @Test
     public void shouldEmbedAPictureWithAnImgTagAndAnEncodedInlineImg() {
+        when(mockCellFormatter.gray(argThat(isEmbeddedImageEncodedInBase64()))).thenReturn("grayed out cell with embedded image");
         RowWrapper<?> row = helper.createTestRow("embed", "");
         fixture.processRow(row);
         verify(row.getCell(1)).body();
-        verify(row.getCell(1)).body(
-                "<img src=\"data:image/svg;base64,PHN2ZyAgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjE"
-                        + "iPg0KICA8ZGVmcz4NCiAgICA8c3R5bGUgdHlwZT0idGV4dC9jc3MiPg0KICAgICAgPCFbQ0RBVEFbDQogICAgICAgIHJlY3QuY2xhc"
-                        + "3Nib3h7DQogICAgICAgICAgc3Ryb2tlICAgICAgIDogYmxhY2s7DQogICAgICAgICAgc3Ryb2tlLXdpZHRoIDogMTsNCiAgICAgICA"
-                        + "gICBmaWxsCQkgICA6IHdoaXRlOw0KICAgICAgICB9DQogICAgICAgIGVsbGlwc2UucGF0dGVybnsNCiAgICAgICAgICBzdHJva2UgI"
-                        + "CAgICAgOiBibGFjazsNCiAgICAgICAgICBzdHJva2Utd2lkdGggOiAxOw0KICAgICAgICAgIGZpbGwJCSAgIDogd2hpdGU7DQogICA"
-                        + "gICAgICAgc3Ryb2tlLWRhc2hhcnJheTogOSw1Ow0KICAgICAgICB9DQogICAgICAgIGxpbmUuY2xhc3Nib3h7DQogICAgICAgICAgc"
-                        + "3Ryb2tlICAgICAgIDogYmxhY2s7DQogICAgICAgICAgc3Ryb2tlLXdpZHRoIDogMTsNCiAgICAgICAgfQ0KICAgICAgICBwb2x5Z29"
-                        + "uLmNsYXNzYm94ew0KICAgICAgICAgIGZpbGwJCSAgIDogd2hpdGU7DQogICAgICAgICAgc3Ryb2tlICAgICAgIDogd2hpdGU7DQogI"
-                        + "CAgICAgICAgc3Ryb2tlLXdpZHRoIDogMDsNCiAgICAgICAgfQ0KICAgICAgICBwb2x5Z29uLm5vdGV7DQogICAgICAgICAgZmlsbAk"
-                        + "JICAgOiB3aGl0ZTsNCiAgICAgICAgICBzdHJva2UgICAgICAgOiBibGFjazsNCiAgICAgICAgICBzdHJva2Utd2lkdGggOiAxOw0KI"
-                        + "CAgICAgICB9DQogICAgICAgIGxpbmUubGlmZWxpbmV7DQogICAgICAgICAgc3Ryb2tlICAgICAgIDogYmxhY2s7DQogICAgICAgICA"
-                        + "gc3Ryb2tlLXdpZHRoIDogMTsNCiAgICAgICAgICBzdHJva2UtZGFzaGFycmF5OiA5LDU7DQogICAgICAgIH0NCiAgICAgICAgcGF0a"
-                        + "C5hcnJvd3sNCiAgICAgICAgICBmaWxsCQkgICA6IG5vbmU7DQogICAgICAgICAgc3Ryb2tlICAgICAgIDogYmxhY2s7DQogICAgICA"
-                        + "gICAgc3Ryb2tlLXdpZHRoIDogMTsNCiAgICAgICAgfQ0KICAgICAgICBwYXRoLmRhc2hhcnJvd3sNCiAgICAgICAgICBmaWxsCQkgI"
-                        + "CA6IG5vbmU7DQogICAgICAgICAgc3Ryb2tlICAgICAgIDogYmxhY2s7DQogICAgICAgICAgc3Ryb2tlLXdpZHRoIDogMTsNCiAgICA"
-                        + "gICAgICBzdHJva2UtZGFzaGFycmF5OiAxMCw3Ow0KICAgICAgICB9DQogICAgICAgIHRleHR7DQogICAgICAgICAgZmlsbCAgICAgI"
-                        + "CAgIDogYmxhY2s7DQogICAgICAgICAgc3Ryb2tlLXdpZHRoIDogMDsNCiAgICAgICAgICBmb250LWZhbWlseSAgOiBzYW5zLXNlcml"
-                        + "mOw0KICAgICAgICB9DQogICAgICAgIHRleHQubmFtZXsNCiAgICAgICAgICBmb250LXNpemUgICAgOiAxMnB4Ow0KICAgICAgICAgI"
-                        + "GZvbnQtd2VpZ2h0ICA6IGJvbGQ7DQogICAgICAgICAgdGV4dC1hbmNob3IgIDogbWlkZGxlOw0KICAgICAgICB9DQogICAgICAgIHR"
-                        + "leHQuaXRlbXsNCiAgICAgICAgICBmb250LXNpemUgICAgOiAxMnB4Ow0KICAgICAgICB9DQogICAgICAgIHRleHQudmlzaWJpbGl0e"
-                        + "XsNCiAgICAgICAgICBmb250LXNpemUgICAgOiAxMnB4Ow0KICAgICAgICAgIGZvbnQtZmFtaWx5ICA6IG1vbm9zcGFjZTsNCiAgICA"
-                        + "gICAgICBmb250LXdlaWdodCAgOiBib2xkOw0KICAgICAgICAgIHRleHQtYW5jaG9yICA6IG1pZGRsZTsNCiAgICAgICAgfQ0KICAgI"
-                        + "CAgICB0ZXh0LmFic3RyYWN0ew0KICAgICAgICAgIGZvbnQtc3R5bGUgICA6IGl0YWxpYzsNCiAgICAgICAgfQ0KICAgICAgICB0ZXh"
-                        + "0Lm1lc3NhZ2V7DQogICAgICAgICAgZm9udC1zaXplICAgIDogMTJweDsNCiAgICAgICAgICB0ZXh0LWFuY2hvciAgOiBzdGFydDsNC"
-                        + "iAgICAgICAgfQ0KICAgICAgICB0ZXh0LmJhY2ttZXNzYWdlew0KICAgICAgICAgIGZvbnQtc2l6ZSAgICA6IDEycHg7DQogICAgICA"
-                        + "gICAgdGV4dC1hbmNob3IgIDogZW5kOw0KICAgICAgICB9DQogICAgICBdXT4NCiAgICA8L3N0eWxlPg0KICANCiAgDQogICAgPG1hc"
-                        + "mtlciBpZD0ibm9IZWFkIiByZWZYPSIwIiByZWZZPSIwIiA+PC9tYXJrZXI+DQogIA0KICAgIDwhLS0gVU1MIFNlcXVlbmNlIFN5bmM"
-                        + "gTWVzc2FnZSBIZWFkIC0tPg0KICAgIDxtYXJrZXIgaWQ9ImNsb3NlZEhlYWQiDQogICAgICB2aWV3Qm94PSIwIDAgMzAgMzAiIHJlZ"
-                        + "lg9IjMwIiByZWZZPSIxNSIgDQogICAgICBtYXJrZXJVbml0cz0ic3Ryb2tlV2lkdGgiDQogICAgICBtYXJrZXJXaWR0aD0iMTAiIG1"
-                        + "hcmtlckhlaWdodD0iMTAiDQogICAgICBvcmllbnQ9ImF1dG8iPg0KICAgICAgPHBhdGggZD0iTSAwIDAgTCAzMCAxNSBMIDAgMzAge"
-                        + "iIgLz4NCiAgICA8L21hcmtlcj4NCiAgICANCiAgICA8IS0tIFVNTCBTZXF1ZW5jZSBBc3luYyBNZXNzYWdlIEhlYWQgLS0+DQogICA"
-                        + "gPG1hcmtlciBpZD0iaGFsZkNsb3NlZEhlYWQiDQogICAgICB2aWV3Qm94PSIwIDAgMzAgMzAiIHJlZlg9IjMwIiByZWZZPSIxNSIgD"
-                        + "QogICAgICBtYXJrZXJVbml0cz0ic3Ryb2tlV2lkdGgiDQogICAgICBtYXJrZXJXaWR0aD0iMTUiIG1hcmtlckhlaWdodD0iMTUiDQo"
-                        + "gICAgICBvcmllbnQ9ImF1dG8iPg0KICAgICAgPHBhdGggZD0iTSAwIDE1IDMwIDE1IEwgMCAzMCB6IiAvPg0KICAgIDwvbWFya2VyP"
-                        + "g0KICAgIA0KICAgIDwhLS0gVU1MIEluaGVyaXRhbmNlIEhlYWQgLS0+DQogICAgPG1hcmtlciBpZD0iZW1wdHlIZWFkIg0KICAgICA"
-                        + "gdmlld0JveD0iMCAwIDMwIDMwIiByZWZYPSIzMCIgcmVmWT0iMTUiIA0KICAgICAgbWFya2VyVW5pdHM9InN0cm9rZVdpZHRoIg0KI"
-                        + "CAgICAgbWFya2VyV2lkdGg9IjE1IiBtYXJrZXJIZWlnaHQ9IjE1Ig0KICAgICAgb3JpZW50PSJhdXRvIj4NCiAgICAgIDxwYXRoIGQ9"
-                        + "Ik0gMCAwIEwgMzAgMTUgTCAwIDMwIHoiIGZpbGw9IndoaXRlIiBzdHJva2U9ImJsYWNrIiAvPg0KICAgIDwvbWFya2VyPg0KICAgI"
-                        + "A0KICAgIDwhLS0gVU1MIEFzc29jaWF0aW9uIEhlYWQgLS0+DQogICAgPG1hcmtlciBpZD0ib3BlbkhlYWQiDQogICAgICB2aWV3Qm94P"
-                        + "SIwIDAgMzAgMzAiIHJlZlg9IjMwIiByZWZZPSIxNSIgDQogICAgICBtYXJrZXJVbml0cz0ic3Ryb2tlV2lkdGgiDQogICAgICBtYX"
-                        + "JrZXJXaWR0aD0iMTAiIG1hcmtlckhlaWdodD0iMTAiDQogICAgICBvcmllbnQ9ImF1dG8iPg0KICAgICAgPHBhdGggZD0iTSAwIDA"
-                        + "gTCAzMCAxNSBMIDAgMzAiIGZpbGw9Im5vbmUiIHN0cm9rZT0iYmxhY2siIC8+DQogICAgPC9tYXJrZXI+DQogICAgDQogICAgPCEt"
-                        + "LSBIZWFkIC0tPg0KICAgIDxtYXJrZXIgaWQ9ImhhbGZPcGVuSGVhZCINCiAgICAgIHZpZXdCb3g9IjAgMCAzMCAzMCIgcmVmWD0iMz"
-                        + "AiIHJlZlk9IjE1IiANCiAgICAgIG1hcmtlclVuaXRzPSJzdHJva2VXaWR0aCINCiAgICAgIG1hcmtlcldpZHRoPSIxNSIgbWFya2Vy"
-                        + "SGVpZ2h0PSIxNSINCiAgICAgIG9yaWVudD0iYXV0byI+DQogICAgICA8cGF0aCBkPSJNIDAgMTUgTCAzMCAxNSBMIDAgMzAiIGZpbG"
-                        + "w9Im5vbmUiIHN0cm9rZT0iYmxhY2siIC8+DQogICAgPC9tYXJrZXI+DQogICAgDQogICAgPCEtLSBVTUwgQWdncmVnYXRpb24gVGFp"
-                        + "bCAtLT4NCiAgICA8bWFya2VyIGlkPSJvcGVuRGlhbW9uZCINCiAgICAgIHZpZXdCb3g9IjAgMCAzMCAzMCIgcmVmWD0iMCIgcmVmWT"
-                        + "0iMTUiIA0KICAgICAgbWFya2VyVW5pdHM9InN0cm9rZVdpZHRoIg0KICAgICAgbWFya2VyV2lkdGg9IjE1IiBtYXJrZXJIZWlnaHQ9"
-                        + "IjE1Ig0KICAgICAgb3JpZW50PSJhdXRvIj4NCiAgICAgIDxwYXRoIGQ9Ik0gMCAxNSBMIDE1IDMgTCAzMCAxNSBMIDE1IDI3IHoiIG"
-                        + "ZpbGw9IndoaXRlIiBzdHJva2U9ImJsYWNrIiAvPg0KICAgIDwvbWFya2VyPg0KICAgIA0KICAgIDwhLS0gVU1MIENvbXBvc2l0aW9u"
-                        + "IFRhaWwgLS0+DQogICAgPG1hcmtlciBpZD0iY2xvc2VkRGlhbW9uZCINCiAgICAgIHZpZXdCb3g9IjAgMCAzMCAzMCIgcmVmWD0iMC"
-                        + "IgcmVmWT0iMTUiIA0KICAgICAgbWFya2VyVW5pdHM9InN0cm9rZVdpZHRoIg0KICAgICAgbWFya2VyV2lkdGg9IjE1IiBtYXJrZXJI"
-                        + "ZWlnaHQ9IjE1Ig0KICAgICAgb3JpZW50PSJhdXRvIj4NCiAgICAgIDxwYXRoIGQ9Ik0gMCAxNSBMIDE1IDMgTCAzMCAxNSBMIDE1ID"
-                        + "I3IHoiIC8+DQogICAgPC9tYXJrZXI+DQogICAgDQogICAgPCEtLSBVTUwgQ29tcG9uZW50IEludGVyZmFjZSBQcm92aWRlZCBIZWFk"
-                        + "IC0tPg0KICAgIDxtYXJrZXIgaWQ9ImVtcHR5Q2lyY2xlIg0KICAgICAgdmlld0JveD0iMCAwIDMwIDMwIiByZWZYPSIxNSIgcmVmWT"
-                        + "0iMTUiIA0KICAgICAgbWFya2VyVW5pdHM9InN0cm9rZVdpZHRoIg0KICAgICAgbWFya2VyV2lkdGg9IjE1IiBtYXJrZXJIZWlnaHQ9"
-                        + "IjE1IiA+DQogICAgIDxjaXJjbGUgY3g9IjE1IiBjeT0iMTUiIHI9IjEwIiBmaWxsPSJ3aGl0ZSIgc3Ryb2tlPSJibGFjayIgc3Ryb2"
-                        + "tlLXdpZHRoPSIxIiAvPg0KICAgIDwvbWFya2VyPg0KICAgIA0KICAgIDwhLS0gVU1MIENvbXBvbmVudCBJbnRlcmZhY2UgUmVxdWly"
-                        + "ZWQgVGFpbCAtLT4NCiAgICA8bWFya2VyIGlkPSJoYWxmQXJjIg0KICAgICAgdmlld0JveD0iMCAwIDMwIDMwIiByZWZYPSIxNSIgcm"
-                        + "VmWT0iMTUiIA0KICAgICAgbWFya2VyVW5pdHM9InN0cm9rZVdpZHRoIg0KICAgICAgbWFya2VyV2lkdGg9IjE1IiBtYXJrZXJIZWln"
-                        + "aHQ9IjE1Ig0KICAgICAgb3JpZW50PSJhdXRvIj4NCiAgICAgPHBhdGggZD0iTSAxNSwwIGExMiwxMiAwIDAsMCAxNSwzMCIgZmlsbD"
-                        + "0ibm9uZSIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSIxIiAvPg0KICAgIDwvbWFya2VyPg0KICAgIA0KICAgIDxkZXNjPkNv"
-                        + "bXBvbmVudCBTeW1ib2w8L2Rlc2M+DQogICAgPHN5bWJvbCBpZD0iQ29tcG9uZW50IiB2aWV3Qm94PSIwIDAgMTYgMjUiID4NCiAgIC"
-                        + "AgIDxyZWN0IHg9IjQiIHk9IjAiIHdpZHRoPSIxMiIgaGVpZ2h0PSIyMCIgZmlsbD0id2hpdGUiIHN0cm9rZT0iYmxhY2siIC8+DQog"
-                        + "ICAgICA8cmVjdCB4PSIwIiB5PSI0IiB3aWR0aD0iOCIgaGVpZ2h0PSI1IiBmaWxsPSJ3aGl0ZSIgc3Ryb2tlPSJibGFjayIgLz4NCi"
-                        + "AgICAgIDxyZWN0IHg9IjAiIHk9IjEyIiB3aWR0aD0iOCIgaGVpZ2h0PSI1IiBmaWxsPSJ3aGl0ZSIgc3Ryb2tlPSJibGFjayIvPg0K"
-                        + "ICAgIDwvc3ltYm9sPg0KICAgIA0KICAgIDxkZXNjPkhpZXJhcmNoeSBTeW1ib2w8L2Rlc2M+DQogICAgPHN5bWJvbCBpZD0iSGllcm"
-                        + "FyY2h5IiB2aWV3Qm94PSIwIDAgMTYgMjUiID4NCiAgICAgIDxyZWN0IHg9IjUiIHk9IjAiIHdpZHRoPSI2IiBoZWlnaHQ9IjYiIGZp"
-                        + "bGw9IndoaXRlIiBzdHJva2U9ImJsYWNrIiAvPg0KICAgICAgPHJlY3QgeD0iMCIgeT0iMTIiIHdpZHRoPSI2IiBoZWlnaHQ9IjYiIG"
-                        + "ZpbGw9IndoaXRlIiBzdHJva2U9ImJsYWNrIi8+DQogICAgICA8cmVjdCB4PSIxMCIgeT0iMTIiIHdpZHRoPSI2IiBoZWlnaHQ9IjYi"
-                        + "IGZpbGw9IndoaXRlIiBzdHJva2U9ImJsYWNrIi8+DQogICAgICA8cGF0aCBkPSJNIDMsMTEgTCAzLDkgTCAxMyw5IEwgMTMsMTEiIG"
-                        + "ZpbGw9Im5vbmUiIHN0cm9rZT0iYmxhY2siIHN0cm9rZS13aWR0aD0iMSIgLz4NCiAgICAgIDxsaW5lIHgxPSI4IiB4Mj0iOCIgeTE9"
-                        + "IjkiIHkyPSI3IiBmaWxsPSJub25lIiBzdHJva2U9ImJsYWNrIiBzdHJva2Utd2lkdGg9IjEiIC8+DQogICAgPC9zeW1ib2w+DQogIC"
-                        + "AgDQogICAgPGRlc2M+RGVsZWdhdGlvbiBTeW1ib2w8L2Rlc2M+DQogICAgPHN5bWJvbCBpZD0iRGVsZWdhdGlvbiIgdmlld0JveD0i"
-                        + "MCAwIDE2IDI1IiA+DQogICAgICA8cmVjdCB4PSIwIiB5PSI1IiB3aWR0aD0iNiIgaGVpZ2h0PSI2IiBmaWxsPSJ3aGl0ZSIgc3Ryb2"
-                        + "tlPSJibGFjayIvPg0KICAgICAgPHJlY3QgeD0iMTAiIHk9IjUiIHdpZHRoPSI2IiBoZWlnaHQ9IjYiIGZpbGw9IndoaXRlIiBzdHJv"
-                        + "a2U9ImJsYWNrIi8+DQogICAgICA8bGluZSB4MT0iNiIgeDI9IjEwIiB5MT0iOCIgeTI9IjgiIGZpbGw9Im5vbmUiIHN0cm9rZT0iYm"
-                        + "xhY2siIHN0cm9rZS13aWR0aD0iMSIgLz4NCiAgICA8L3N5bWJvbD4NCiAgICANCiAgICA8ZGVzYz5SYWRpYWwgU3ltYm9sPC9kZXNj"
-                        + "Pg0KICAgIDxzeW1ib2wgaWQ9IlJhZGlhbCIgdmlld0JveD0iMCAwIDE2IDI1IiA+DQogICAgICA8cmVjdCB4PSI2IiB5PSIwIiB3aW"
-                        + "R0aD0iNSIgaGVpZ2h0PSI1IiBmaWxsPSJ3aGl0ZSIgc3Ryb2tlPSJibGFjayIvPg0KICAgICAgPHJlY3QgeD0iMCIgeT0iNiIgd2lk"
-                        + "dGg9IjUiIGhlaWdodD0iNSIgZmlsbD0id2hpdGUiIHN0cm9rZT0iYmxhY2siLz4NCiAgICAgIDxyZWN0IHg9IjEyIiB5PSI2IiB3aW"
-                        + "R0aD0iNSIgaGVpZ2h0PSI1IiBmaWxsPSJ3aGl0ZSIgc3Ryb2tlPSJibGFjayIvPg0KICAgICAgPHJlY3QgeD0iNiIgeT0iMTQiIHdp"
-                        + "ZHRoPSI1IiBoZWlnaHQ9IjUiIGZpbGw9IndoaXRlIiBzdHJva2U9ImJsYWNrIi8+DQogICAgICA8bGluZSB4MT0iOCIgeTE9IjE0Ii"
-                        + "B4Mj0iOCIgeTI9IjUiIGZpbGw9Im5vbmUiIHN0cm9rZT0iYmxhY2siIHN0cm9rZS13aWR0aD0iMSIgLz4NCiAgICAgIDxsaW5lIHgx"
-                        + "PSI4IiB5MT0iMTQiIHgyPSIzIiB5Mj0iMTEiIGZpbGw9Im5vbmUiIHN0cm9rZT0iYmxhY2siIHN0cm9rZS13aWR0aD0iMSIgLz4NCi"
-                        + "AgICAgIDxsaW5lIHgxPSI4IiB5MT0iMTQiIHgyPSIxMiIgeTI9IjExIiBmaWxsPSJub25lIiBzdHJva2U9ImJsYWNrIiBzdHJva2Ut"
-                        + "d2lkdGg9IjEiIC8+DQogICAgPC9zeW1ib2w+DQogICAgDQogICAgPGRlc2M+RmxvdyBTeW1ib2w8L2Rlc2M+DQogICAgPHN5bWJvbC"
-                        + "BpZD0iRmxvdyIgdmlld0JveD0iMCAwIDE2IDI1IiA+DQogICAgICA8cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iNSIgaGVpZ2h0PSI1"
-                        + "IiBmaWxsPSJ3aGl0ZSIgc3Ryb2tlPSJibGFjayIvPg0KICAgICAgPHJlY3QgeD0iOSIgeT0iMCIgd2lkdGg9IjUiIGhlaWdodD0iNS"
-                        + "IgZmlsbD0id2hpdGUiIHN0cm9rZT0iYmxhY2siLz4NCiAgICAgIDxyZWN0IHg9IjAiIHk9IjgiIHdpZHRoPSI1IiBoZWlnaHQ9IjUi"
-                        + "IGZpbGw9IndoaXRlIiBzdHJva2U9ImJsYWNrIi8+DQogICAgPC9zeW1ib2w+DQogICAgDQogIDwvZGVmcz4NCiAgDQoKPGRlc2M+QW"
-                        + "dlbnRzIGFuZCBsaWZlbGluZXMgb2YgdGhlIHNlcXVlbmNlIGRpYWdyYW08L2Rlc2M+Cgo8ZGVzYz5NZXNzYWdlczwvZGVzYz4NCiAgDQo8L3N2Zz4NCg==\" />");
+        verify(row.getCell(1)).body("grayed out cell with embedded image");
         verifyNoMoreInteractions(row.getCell(1));
         verifyNoMoreInteractions(mockModel);
+    }
+
+    private IsEmbeddedImageEncodedInBase64 isEmbeddedImageEncodedInBase64() {
+        return new IsEmbeddedImageEncodedInBase64();
     }
 
     @Test
