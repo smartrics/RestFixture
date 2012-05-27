fdb743068596f28fe728c7410fe882521b1f0e34
smartrics <smartrics@gmail.com>
2012-05-27 16:02:03 +0100

    added (c) note where missing

--- src/main/java/smartrics/rest/fitnesse/fixture/support/http/DeleteMethod.java
+++ src/main/java/smartrics/rest/fitnesse/fixture/support/http/DeleteMethod.java
@@ -1,3 +1,23 @@
+/*  Copyright 2012 Fabrizio Cannizzo
+ *
+ *  This file is part of RestFixture.
+ *
+ *  RestFixture (http://code.google.com/p/rest-fixture/) is free software:
+ *  you can redistribute it and/or modify it under the terms of the
+ *  GNU Lesser General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  RestFixture is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with RestFixture.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  If you want to contact the author please leave a comment here
+ *  http://smartrics.blogspot.com/2008/08/get-fitnesse-with-some-rest.html
+ */
 package smartrics.rest.fitnesse.fixture.support.http;
 
 import org.apache.commons.httpclient.HostConfiguration;
